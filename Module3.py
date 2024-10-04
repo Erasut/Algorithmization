a7f2235df10d17aafd6a28a54d6a66c8ef128dda
Erasut <154245966+Erasut@users.noreply.github.com>
2024-10-04 11:24:05 +0300

    Create Module3.py

---
+++ Module3.py
@@ -0,0 +1,48 @@
+#Задание 1
+phrase = "Programming is fun!"
+substring = phrase[0:11]
+print(substring)
+substring1 = phrase[13]
+print(substring1)
+substring2 = phrase[18]
+print(substring2)
+
+#Задание 2
+numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+element1 = numbers[3]
+print(element1)
+element2 = numbers[-1]
+print(element2)
+element3 = numbers[-6]
+print(element3)
+
+#Задание 3
+cities = ("New York", "London", "Paris", "Tokyo", "Sydney")
+gorod = cities[1]
+print(gorod)
+gorod1 = cities[-3]
+print(gorod1)
+gorod2 = cities[-5]
+print(gorod2)
+
+#Задание 4
+quote = "The quick brown fox jumps over the lazy dog"
+pod = quote[0:5]
+print(pod)
+pod1 = quote[10:19]
+print(pod1)
+pod2 = quote[::2]
+print(pod2)
+reversed_list = quote[::-1]
+print(reversed_list)
+
+#Задание 5
+numbers = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+pods = numbers[0:4]
+print(pods)
+pods2 = numbers[5:8]
+print(pods2)
+pods3 = numbers[::2]
+print(pods3)
+slice = numbers[::-1]
+print(slice)
