cf4096d385e9f5f22d7bf14fd70207a314e3da26
Erasut <154245966+Erasut@users.noreply.github.com>
2024-10-08 13:33:38 +0300

    Create Module 5.py

---
+++ Module 5.py
@@ -0,0 +1,23 @@
+#Задание 1
+print("Введите ваш возраст:")
+age=int(input())
+print("Введите возраст вашего друга:")
+age_friend=int(input())
+print("Сумма ваших возрастов:")
+print(age+age_friend)
+
+#Задание 2
+print("Введите ваше имя:")
+name=input()
+print("Введите вашу фамилию:")
+surname=input()
+print("Привет,", name, surname, "!")
+
+#Задание 3
+print("Меню:\n1. Суп\n2. Паста\n3. Десертик")
+#Задание 4
+print("****\n****\n****")
+#Задание 5
+print("Число:")
+x=int(input())
+print(f'{x-3} {x-2} {x-1}\n{x}\n{x+3} {x+2} {x+1}')
