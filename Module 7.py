c0156d30ea2970573f200ae59c172e3f5b53bc86
Erasut <154245966+Erasut@users.noreply.github.com>
2024-11-22 12:16:24 +0300

    Update Module 7.py

--- Module 7.py
+++ Module 7.py
@@ -1,5 +1,5 @@
 #Задание 1
-def create_car(brand: str, color: str, max_speed: int) -> str:
+def create_car(brand: str, color: str, max_speed: int):
     return f"Марка: {brand} Цвет: {color} Максимальная скорость: {max_speed} км/ч"
 car_info = create_car("Mercedes", "Черный", 350)
 print(car_info)
