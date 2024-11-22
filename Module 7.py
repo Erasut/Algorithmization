15f19dcb99da6268cdaa5b2959a5336aadf8efb9
Erasut <154245966+Erasut@users.noreply.github.com>
2024-11-22 12:19:06 +0300

    Update Module 7.py

--- Module 7.py
+++ Module 7.py
@@ -18,3 +18,19 @@ switch_3 = None
 switch_check(switch_1)
 switch_check(switch_2)
 switch_check(switch_3)
+
+#Задание 3
+def triangle_type(side1: float, side2: float, side3: float) -> None:
+    if (side1 + side2 > side3) and (side1 + side3 > side2) and (side2 + side3 > side1):
+        if side1 == side2 == side3:
+            print("Равносторонний треугольник")
+        elif side1 == side2 or side1 == side3 or side2 == side3:
+            print("Равнобедренный треугольник")
+        else: 
+            print("Разносторонний треугольник")
+    else: 
+        print("Некорректные стороны. Невозможно построить треугольник.")
+triangle_type(3, 3, 3)
+triangle_type(5, 5, 8)
+triangle_type(3, 4, 5)
+triangle_type(1, 2, 3)
