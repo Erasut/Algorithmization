0fc1ce27487e2b496d9f9cccd10c604e6efaef1b
Erasut <154245966+Erasut@users.noreply.github.com>
2025-06-25 12:51:17 +0300

    Update Practice Exercices 10.py

--- Practice Exercices 10.py
+++ Practice Exercices 10.py
@@ -1,22 +1,23 @@
 #Задание 2
 def punct(txt):
-    count = sum(1 for char in txt if char in "!?.(),")
+    count = sum(1 for str in txt if str in "!?.(),")
     print(count)
 
 punct('(Как дела?)')
-----------------------------------------
+
 #Задание 3
 def create_cube(x, y):
     for _ in range(y):
         print('*' * x)
 
 create_cube(2, 3)
----------------------------------------
+
 #Задание 1 
 def upper(t):
-    for char in t:
-        if char.isupper():
-            print(char, end=' ')
+    for str in t:
+        if str.isupper():
+            print(str, end=' ')
     print()
 
 upper('PriVet')
+
